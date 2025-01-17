e9906ffac8293d9d35ab590cf37561dd46c56d51
fti-Asd <ftiasg1401@gmail.com>
2025-01-17 22:17:10 +0330

    #feature: functions are seperated

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,32 @@
 // const tabsUl = document.querySelectorAll(".map-tabs");
 const mapTabContentDiv = document.querySelector("#map-tabContent");
 
+let mapList = [];
+let newMap;
+let lastLocation= { latitude: "", longitude: "" };;
+
+
 locations.forEach((item, index) => {
 
-  document.querySelectorAll(".map-tabs").forEach((tabUl) => {
-    tabUl.innerHTML += `<li class="nav-item d-flex justify-content-end" data-bs-dismiss="offcanvas" role="presentation">
+  document.querySelectorAll(".map-tabs:not(.navbar .map-tabs)").forEach((tabLi) => {
+    tabLi.innerHTML += `<li class="nav-item d-flex justify-content-end" role="presentation">
                           <button class="nav-link w-75 text-end text-black ${index === 0 ? "active bg-warning text-white" : ""}" id="map-btn-${index}" data-bs-toggle="pill" data-bs-target="#map-pane-${index}" type="button" role="tab" aria-controls="map-pane-${index}" aria-selected="${index === 0}">
                             ${item.place}
                           </button>
                         </li>`;
   });
 
+  document.querySelectorAll(".navbar .map-tabs").forEach((tabLi) => {
+    tabLi.innerHTML += `<li class="nav-item d-flex justify-content-end" data-bs-dismiss="offcanvas" role="presentation">
+                          <button class="nav-link w-75 text-end text-black ${index === 0 ? "active bg-warning text-white" : ""}" id="map-btn-${index}" data-bs-toggle="pill" data-bs-target="#map-pane-${index}" type="button" role="tab" aria-controls="map-pane-${index}" aria-selected="${index === 0}">
+                            ${item.place}
+                          </button>
+                        </li>`;
+  });
 
-    mapTabContentDiv.innerHTML += `<div class="tab-pane fade ${index === 0 ? "show active" : ""}" id="map-pane-${index}" role="tabpanel" aria-labelledby="map-btn-${index}" tabindex="0">
-                                      <div id="map-container"></div>
-                                    </div>`;
+  mapTabContentDiv.innerHTML += `<div class="tab-pane fade ${index === 0 ? "show active" : ""}" id="map-pane-${index}" role="tabpanel" aria-labelledby="map-btn-${index}" tabindex="0">
+                                    <div id="map-container"></div>
+                                  </div>`;
 });
 
 document.querySelectorAll(".map-tabs").forEach((tabUl)=>{
--- assets/js/createSharedMap.js
+++ assets/js/createSharedMap.js
@@ -1,7 +1,3 @@
-let mapList = [];
-let newMap;
-let lastLocation= { latitude: "", longitude: "" };;
-
 function createSharedMap(mapContainer, location) {
     if (!mapList.some(map => map.container === mapContainer)) {
         newMap = L.map(mapContainer).setView(location, 17);
@@ -26,31 +22,4 @@ function createSharedMap(mapContainer, location) {
         const existingMap = mapList.find(map => map.container === mapContainer);
         existingMap.map.setView(location, 17);
     }
-}
-
-function addMarker(object){
-    let dornikaIcon = L.icon({
-        iconUrl: object.iconUrl,
-        iconSize: [40, 42], 
-        iconAnchor: [20, 20], 
-        popupAnchor: [0, -20],    
-    });
-
-    L.marker(object.location, { icon: dornikaIcon })
-    .addTo(newMap)
-    .bindPopup(object.popUpText).openPopup();
-}
-
-function mapViewHandler(index=0){
-    const mapContainer = document.getElementById(`map-pane-${index}`);
-    if (mapContainer) {
-        createSharedMap(mapContainer, locations[index].location);
-
-        addMarker(locations[index]);
-    }
-}
-
-
-function getLocation(callback) {
-    callback(lastLocation)
 }
